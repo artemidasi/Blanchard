546790b734e0b10062e79e79418aa6f5882652a9
Maks <maxartem0419@gmail.com>
2021-03-06 20:51:04 +0300

    script update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 "use strict";
 
 // Const
-
 const SPEED_SWIPER = 4000;
-
 const swiperNavigateNext = document.querySelector('.gallery-navigate__next');
 const swiperNavigatePrev = document.querySelector('.gallery-navigate__prev');
 const editionPrice = document.querySelector(".editions-left__priceContainer");
